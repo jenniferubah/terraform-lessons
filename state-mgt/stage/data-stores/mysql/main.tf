27da37428a1064c03ae01be26a032a0c4bc57b6d
JenniferUbah <cju.cipher@gmail.com>
2024-04-04 12:54:20 -0400

    updated db config

--- state-mgt/stage/data-stores/mysql/main.tf
+++ state-mgt/stage/data-stores/mysql/main.tf
@@ -2,17 +2,20 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_db_instance" "my_db" {
+resource "aws_db_instance" "example" {
   instance_class      = "db.t2.micro"
   identifier_prefix   = "terraform-example-db"
   engine              = "mysql"
   allocated_storage   = 10
   skip_final_snapshot = true
-  engine_version = "5.6.27"
+#   engine_version = "8.0"
   db_name             = "my_database"
 
-  username = var.db_username
-  password = var.db_password
+
+  username = "rdsdb"
+  password = "rdspassword"
+#   username = var.db_username
+#   password = var.db_password
 }
 
 terraform {
--- state-mgt/stage/data-stores/mysql/outputs.tf
+++ state-mgt/stage/data-stores/mysql/outputs.tf
@@ -1,9 +1,9 @@
 output "address" {
-  value       = aws_db_instance.my_db.address
+  value       = aws_db_instance.example.address
   description = "Connect to the database at this endpoint"
 }
 
 output "port" {
-  value = aws_db_instance.my_db.port
+  value = aws_db_instance.example.port
   description = "The port the database is listening on"
 }
